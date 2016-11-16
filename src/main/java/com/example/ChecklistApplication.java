8ed5a8febb44f9c33d65f2cc510eb37aaea13bb6
Zev Schwartz <zev@pbxcom.net>
2016-11-15 22:43:36 -0500

    Moved main application class

--- src/main/java/com/example/ChecklistApplication.java
+++ src/main/java/com/example/ChecklistApplication.java
@@ -1,10 +1,7 @@
-package com.example.checklist;
+package com.example;
 
-import com.example.checklist.db.auditing.SpringSecurityAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
