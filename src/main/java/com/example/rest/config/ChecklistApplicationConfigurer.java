4c8e023e65eddb9cc5a1f40eb67af7ea5703883c
Zev Schwartz <zev@pbxcom.net>
2016-11-20 20:17:57 -0500

    Working with Oauth2

--- src/main/java/com/example/rest/config/ChecklistApplicationConfigurer.java
+++ src/main/java/com/example/rest/config/ChecklistApplicationConfigurer.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableJpaAuditing
+@EnableResourceServer
 public class ChecklistApplicationConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -34,6 +36,7 @@ public class ChecklistApplicationConfigurer extends WebSecurityConfigurerAdapter
                 .and()
             .formLogin()
                 .loginPage("/").permitAll()
+                .and()
             ;
     }
 
@@ -46,4 +49,5 @@ public class ChecklistApplicationConfigurer extends WebSecurityConfigurerAdapter
     public AuditorAware<User> auditorProvider() {
         return new SpringSecurityAuditorAware();
     }
+
 }
---
+++ src/main/java/com/example/rest/config/Oauth2Configuration.java
@@ -0,0 +1,39 @@
+package com.example.rest.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+/**
+ * Created by czrif on 11/20/2016.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class Oauth2Configuration extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public Oauth2Configuration(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .authorizedGrantTypes("refresh_token", "password")
+                .scopes("openid");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(this.authenticationManager);
+    }
+}
